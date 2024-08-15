7c6230157bd80cf7736409c97a57c1959ee995b6
unknown <ar.bustos@duouc.cl>
2024-08-14 20:46:11 -0400

    subiendo primer desafio spring

---
+++ Desafio-Gestor_de_proyectos/GestorDeProyectos/src/main/java/cl/praxis/GestorDeProyectos/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cl.praxis.GestorDeProyectos;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestorDeProyectosApplication.class);
+	}
+
+}
